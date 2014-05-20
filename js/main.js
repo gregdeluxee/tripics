7af50c522beeb14c5cd00bde48cafa40c2426d44
gregdeluxee <gregdeluxee@gmail.com>
2014-05-20 02:47:22 +0200

    test camera

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 //START Zepto
 ;(function($){
 
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+document.addEventListener("deviceready",onDeviceReady,false);
+
+function onDeviceReady() {
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
+
+
 
 var searchMenu = document.querySelector("#menu input[type=text]");
 searchMenu.onfocus=function(){
